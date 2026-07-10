b0a6ad274f71f8a535a82049ddb34fdc0f4e50fd
Anna Oake <anna@oa.ke>
2026-07-10 17:29:20 +0200

    flake: drop macOS, target OpenRGB 1.0rc3

--- flake.nix
+++ flake.nix
@@ -2,13 +2,9 @@
   description = "OpenRGB Steam Sink plugin devshell";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  inputs.systems.url = "github:nix-systems/default";
-  inputs.flake-utils = {
-    url = "github:numtide/flake-utils";
-    inputs.systems.follows = "systems";
-  };
+  inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.openrgb-src = {
-    url = "gitlab:CalcProgrammer1/OpenRGB?ref=release_candidate_1.0rc2";
+    url = "gitlab:CalcProgrammer1/OpenRGB?ref=release_candidate_1.0rc3";
     flake = false;
   };
 
@@ -19,23 +15,18 @@
       openrgb-src,
       ...
     }:
-    flake-utils.lib.eachSystem [ "aarch64-darwin" "x86_64-linux" ] (
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
         devShells.default = pkgs.mkShell {
-          packages =
-            with pkgs;
-            [
-              gnumake
-              pkg-config
-              qt6.qtbase
-            ]
-            ++ lib.optionals stdenv.hostPlatform.isLinux [
-              openrgb
-            ];
+          packages = with pkgs; [
+            gnumake
+            pkg-config
+            qt6.qtbase
+          ];
 
           OPENRGB_SOURCE_DIR = openrgb-src;
         };
